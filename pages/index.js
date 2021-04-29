843cd79aa434212c07b633d2b816a642e40edcb6
ary.medeiros-at-424057606549 <ary.medeiros@erpflex.com.br>
2021-04-29 18:47:20 -0300

    1.02

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { config, dom } from "@fortawesome/fontawesome-svg-core";
 import  Tabs  from "../components/Tabs";
 import dynamic from 'next/dynamic';
-
+import detectMobile from '../components/util/detectMobile';
 
 config.autoAddCss = false;
 
@@ -23,31 +23,35 @@ const DynamicLooper = dynamic(
 
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Musical Assistant</title>
-        <link rel="icon" href="/favicon.ico" />
-        <style>{dom.css()}</style>
-      </Head>
+  const detect = detectMobile();
+  if ( detect.isMobile() ){
+    return (
+      <div className="container">
+        <Head>
+          <title>Musical Assistant</title>
+          <link rel="icon" href="/favicon.ico" />
+          <style>{dom.css()}</style>
+        </Head>
 
-      <main className="main">
-        <div id="tuner" className="panel active">
-          <DynamicTuner />
-        </div>
-        <div id="metronome" className="panel">
-          <DynamicMetronome />
-        </div>
-        <div id="loop" className="panel">
-          <DynamicLooper />
-        </div>
-      </main>
+        <main className="main">
+          <div id="tuner" className="panel active">
+            <DynamicTuner />
+          </div>
+          <div id="metronome" className="panel">
+            <DynamicMetronome />
+          </div>
+          <div id="loop" className="panel">
+            <DynamicLooper />
+          </div>
+        </main>
 
-      <footer className="footer">
-        <div className="grow"></div>
-        <Tabs></Tabs>
-        <div className="grow"></div>        
-      </footer>
-    </div>
-  )
+        <footer className="footer">
+          <div className="grow"></div>
+          <Tabs></Tabs>
+          <div className="grow"></div>        
+        </footer>
+      </div>
+    )
+  }
+  return ( "<h1>Landing Page</h1>");
 }
